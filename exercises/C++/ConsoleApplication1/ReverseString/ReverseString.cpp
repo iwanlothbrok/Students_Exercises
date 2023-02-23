7dda0ba38fcbaf05a4a1ce1a2ab5fc13fe40bbd2
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-02-23 18:02:07 +0200

    more exercises c++

---
+++ exercises/C++/ConsoleApplication1/ReverseString/ReverseString.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	string input = "Hello World!";
+
+	int len = input.length();
+
+	// делим на се взимат двете части на стринг
+	for (int i = 0; i < len / 2; i++)
+	{
+		char temp = input[i];
+		input[i] = input[len - i - 1];
+		input[len - i - 1] = temp;
+	}
+
+	cout << input;
+}
