a7d9596dffe313978645c4981f5d8031cbc30633
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-03-13 11:37:31 +0200

    more exr were added

---
+++ exercises/C++/ASP.NET_AddNumbers/ASP.NET_AddNumbers/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_AddNumbers.Controllers
+{
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+	}
+}
