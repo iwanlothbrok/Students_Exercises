556f637fde4ac0149ac46763ac2528e547764aa9
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-03-16 13:22:30 +0200

    is prime and it has not zeros in it check

---
+++ exercises/C++/MathExercise/IsPrimeAndIsWithoutZero/IsPrimeAndIsWithoutZero/IsPrimeAndIsWithoutZero.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+using namespace std;
+bool hasZeroDigit(int num);
+bool isMultiple(int num);
+int main()
+{
+
+	int num;
+	bool valid = false;
+	do
+	{
+		cin >> num;
+
+		if (num != 0 && hasZeroDigit(num) == true && isMultiple(num) == true) {
+			valid = true;
+			cout << num << " ";
+		}
+	} while (num != 0); {
+		if (valid == false) {
+			cout << "NO";
+		}
+	}
+
+	return 0;
+}
+
+bool hasZeroDigit(int num)
+{
+	while (num != 0)
+	{
+		if (num % 10 == 0)
+		{
+			return false;
+		}
+		num /= 10;
+	}
+	return true;
+}
+
+bool isMultiple(int num)
+{
+	int digit;
+
+	// setvame promenliva sys stoinosta na numbera, zashtoto numbera se promenq a na nas ni trqbwa ne promenenata stoinost
+	int staticNumberValue = num;
+	while (num != 0)
+	{
+		digit = num % 10;
+
+		if (digit == 0 || staticNumberValue % digit != 0)
+		{
+			return false;
+		}
+
+		num /= 10;
+	}
+	return true;
+}
