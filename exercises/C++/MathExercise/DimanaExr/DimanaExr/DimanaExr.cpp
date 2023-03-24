8581af03f95df83618a36899f032c2a357f0f654
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-03-24 09:56:00 +0200

    some c++ exr

---
+++ exercises/C++/MathExercise/DimanaExr/DimanaExr/DimanaExr.cpp
@@ -0,0 +1,41 @@
+// DimanaExr.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+void Swap(int& first, int& second);
+int main()
+{
+    int ch1, ch2;
+    do
+    {
+        cin >> ch1 >> ch2;
+
+        if (ch1 != 0 || ch2 != 0)
+        {
+            Swap(ch1, ch2);
+            cout << ch1 << ' ' << ch2 << endl;
+        }
+    } while (ch1 != 0 || ch2 != 0);
+    return 0;
+}
+
+void Swap(int& first, int& second)
+{
+    int temp = first;
+    first = second;
+    second = temp;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ exercises/C++/MathExercise/DimanaExr/Exr02/Exr02.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+void SortAscending(int& a, int& b, int& c);
+int main()
+{
+    int ch1, ch2, ch3;
+
+
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> ch1 >> ch2 >> ch3;
+
+        SortAscending(ch1, ch2, ch3);
+
+        cout << ch1 << ' ' << ch2 << ' ' << ch3 << endl;
+    }
+    return 0;
+}
+void SortAscending(int& a, int& b, int& c)
+{
+    if (a > b)
+    {
+        swap(a, b);
+    }
+    if (a > c)
+    {
+        swap(a, c);
+    }
+    if (b > c)
+    {
+        swap(b, c);
+    }
+}
