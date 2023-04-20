4e669ff5c5ede8b103e8f0792c0811ba75d34199
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-04-20 16:07:43 +0300

    first express server

---
+++ exercises/JS/catgram/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) => {
+console.log('Hello from console');
+
+res.send('Hello world from express!')
+})
+
+app.listen(5000,() => console.log('Server is listening on port 5000'));
