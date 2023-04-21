bc25036c4ca73e13ebcdc04e7c0bd1d66f81f605
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-04-21 14:45:50 +0300

    first test for adding task is working properly

--- Svetlina/TaskBoard/TaskBoard-Skeleton/TaskBoard-Skeleton/TaskBoard.WebApp.UnitTests/TasksControllerTests.cs
+++ Svetlina/TaskBoard/TaskBoard-Skeleton/TaskBoard-Skeleton/TaskBoard.WebApp.UnitTests/TasksControllerTests.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Net;
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using NUnit.Framework.Internal.Execution;
+using System;
 using System.Linq;
-
-using TaskBoard.Data;
 using TaskBoard.Tests.Common;
 using TaskBoard.WebApp.Controllers;
 using TaskBoard.WebApp.Models.Task;
-
-using NUnit.Framework;
-using Microsoft.AspNetCore.Mvc;
 
 namespace TaskBoard.WebApp.UnitTests
 {
@@ -29,7 +26,34 @@ namespace TaskBoard.WebApp.UnitTests
         [Test]
         public void Test_Create_PostValidData()
         {
+            // Arrange
+            int taskCountBefore = this.dbContext.Tasks.Count();
+
+
+            var newTask = new TaskFormModel
+            {
+                Title = "Test Task" + DateTime.Now.Ticks,
+                Description = "Task to test if the task creation is succsessful",
+                BoardId = this.testDb.OpenBoard.Id,
+            };
+
+            // Act
+            var result = this.controller.Create(newTask);
+
+			// Assert redirect
+			var redirectResult = result as RedirectToActionResult;
+            Assert.AreEqual("Boards", redirectResult.ControllerName);
+            Assert.AreEqual("All", redirectResult.ActionName);
+
+            // Assert count 
+            int taskCounterAfter = this.dbContext.Tasks.Count();
+            Assert.AreEqual(taskCountBefore + 1, taskCounterAfter);
+
+            // Assert if is added
+            var newTaskInDb = this.dbContext.Tasks.FirstOrDefault(t=>t.Title == newTask.Title);
+            Assert.IsTrue(newTaskInDb.Id > 0);
         }
+
 
         [Test]
         public void Test_DeletePage_ValidId()
