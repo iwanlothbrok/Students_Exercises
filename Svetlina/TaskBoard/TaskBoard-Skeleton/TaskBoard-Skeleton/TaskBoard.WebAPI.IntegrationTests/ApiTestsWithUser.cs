2183ac14a92ca22d56015f1b44a8c2d2fbaa73d0
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-04-21 15:54:48 +0300

    first and third test in API tests were added

--- Svetlina/TaskBoard/TaskBoard-Skeleton/TaskBoard-Skeleton/TaskBoard.WebAPI.IntegrationTests/ApiTestsWithUser.cs
+++ Svetlina/TaskBoard/TaskBoard-Skeleton/TaskBoard-Skeleton/TaskBoard.WebAPI.IntegrationTests/ApiTestsWithUser.cs
@@ -24,16 +24,51 @@ namespace TaskBoard.WebAPI.IntegrationTests
         [Test]
         public async Task Test_Tasks_GetTaskById_ValidId()
         {
+            //Arrange
+            var cssTaskId = this.testDb.CSSTask.Id;
+
+            //Act
+            var respond = await this.httpClient.GetAsync($"/api/tasks/{cssTaskId}");
+
+            //Assert
+            Assert.AreEqual(respond.StatusCode, HttpStatusCode.OK);
+
+            var respondContent = respond.Content.
+                ReadAsAsync<TaskExtendedListingModel>()
+                .Result;
+
+            Assert.AreEqual(this.dbContext.Tasks.Find(cssTaskId).Title , respondContent.Title);
         }
 
         [Test]
         public async Task Test_Tasks_CreateTask_ValidData()
         {
+
         }
 
         [Test]
         public async Task Test_Tasks_EditTask_ValidId()
         {
+            //Arrange
+            var editTask = this.testDb.EditTask;
+
+            var changedTitle = "Changed CSS Task";
+            var changedTask = new TaskBindingModel()
+            {
+                Title = changedTitle,
+                Description = editTask.Description,
+                Board = editTask.Board.Name
+            };
+
+            //Act
+            var putRespond = await this.httpClient.PutAsJsonAsync($"/api/tasks/{editTask.Id}", changedTask);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.NoContent, putRespond.StatusCode);
+
+            this.dbContext = this.testDb.CreateDbContext();
+            var taskInDbAfter = this.dbContext.Tasks.Find(editTask.Id);
+            Assert.AreEqual(changedTitle,taskInDbAfter.Title);
         }
 
         [Test]
