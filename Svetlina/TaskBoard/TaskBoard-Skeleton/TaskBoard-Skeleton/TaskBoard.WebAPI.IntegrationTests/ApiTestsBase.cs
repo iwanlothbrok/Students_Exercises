c85f29b9343ef328767400f0a75ab0e24e42805c
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-04-21 12:51:50 +0300

    taskBoard exer was added

---
+++ Svetlina/TaskBoard/TaskBoard-Skeleton/TaskBoard-Skeleton/TaskBoard.WebAPI.IntegrationTests/ApiTestsBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+
+using TaskBoard.Data;
+using TaskBoard.Tests.Common;
+using TaskBoard.WebAPI.Models.User;
+using TaskBoard.WebAPI.Models.Response;
+
+using NUnit.Framework;
+
+namespace TaskBoard.WebAPI.IntegrationTests
+{
+    public class ApiTestsBase
+    {
+        protected TestDb testDb;
+        protected ApplicationDbContext dbContext;
+        protected TestTaskBoardApp<Startup> testTaskBoardApi;
+        protected HttpClient httpClient;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUpBase()
+        {
+            this.testDb = new TestDb();
+            this.dbContext = this.testDb.CreateDbContext();
+            this.testTaskBoardApi = new TestTaskBoardApp<Startup>(
+                this.testDb, "../../../../TaskBoard.WebAPI");
+            this.httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(this.testTaskBoardApi.ServerUri)
+            };
+        }
+
+        public async System.Threading.Tasks.Task AuthenticateAsync()
+        {
+            this.httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("bearer", await this.GetJWTAsync());
+        }
+
+        private async Task<string> GetJWTAsync()
+        {
+            var userMaria = this.testDb.UserMaria;
+            var response = await this.httpClient.PostAsJsonAsync("api/users/login",
+                new LoginModel
+                {
+                    Username = userMaria.UserName,
+                    Password = userMaria.UserName
+                });
+
+            var loginResponse = await response.Content.ReadAsAsync<ResponseWithToken>();
+
+            return loginResponse.Token;
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDownBase()
+        {
+            // Stop and dispose the local Web API server
+            this.testTaskBoardApi.Dispose();
+        }
+    }
+}
---
+++ Svetlina/TaskBoard/TaskBoard-Skeleton/TaskBoard-Skeleton/TaskBoard.WebApp.SeleniumTests/SeleniumTestsBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+
+using TaskBoard.Tests.Common;
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TaskBoard.WebApp.SeleniumTests
+{
+    public abstract class SeleniumTestsBase
+    {
+        protected TestDb testDb;
+        protected IWebDriver driver;
+        protected TestTaskBoardApp<Startup> testTaskBoardApp;
+        protected string baseUrl;
+        protected string username = "user" + DateTime.Now.Ticks.ToString().Substring(10);
+        protected string password = "pass" + DateTime.Now.Ticks.ToString().Substring(10);
+
+        [OneTimeSetUp]
+        public void OneTimeSetupBase()
+        {
+            // Run the Web app in a local Web server
+            this.testDb = new TestDb();
+            this.testTaskBoardApp = new TestTaskBoardApp<Startup>(
+                this.testDb, "../../../../TaskBoard.WebApp");
+            this.baseUrl = this.testTaskBoardApp.ServerUri;
+
+            // Setup the ChromeDriver
+            var chromeOptions = new ChromeOptions();
+            if (!Debugger.IsAttached)
+                chromeOptions.AddArguments("headless");
+            chromeOptions.AddArguments("--start-maximized");
+            this.driver = new ChromeDriver(chromeOptions);
+
+            // Set an implicit wait for the UI interaction
+            this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDownBase()
+        {
+            // Stop and dispose the Selenium driver
+            this.driver.Quit();
+
+            // Stop and dispose the local Web server
+            this.testTaskBoardApp.Dispose();
+        }
+    }
+}
