0a35372f09cdae3753b0905b9d5b70595b9f603c
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-04-24 09:24:38 +0300

    Every Api test is working

--- Svetlina/TaskBoard/TaskBoard-Skeleton/TaskBoard-Skeleton/TaskBoard.WebAPI.IntegrationTests/ApiTestsWithUser.cs
+++ Svetlina/TaskBoard/TaskBoard-Skeleton/TaskBoard-Skeleton/TaskBoard.WebAPI.IntegrationTests/ApiTestsWithUser.cs
@@ -10,19 +10,20 @@ using TaskBoard.WebAPI.Models.Response;
 
 using NUnit.Framework;
 using Newtonsoft.Json;
+using TaskBoard.Data;
 
 namespace TaskBoard.WebAPI.IntegrationTests
 {
     public class ApiTestsWithUser : ApiTestsBase
     {
         [OneTimeSetUp]
-        public async Task OneTimeSetUp()
+        public async System.Threading.Tasks.Task OneTimeSetUp()
         {
             await base.AuthenticateAsync();
         }
 
         [Test]
-        public async Task Test_Tasks_GetTaskById_ValidId()
+        public async System.Threading.Tasks.Task Test_Tasks_GetTaskById_ValidId()
         {
             //Arrange
             var cssTaskId = this.testDb.CSSTask.Id;
@@ -41,13 +42,30 @@ namespace TaskBoard.WebAPI.IntegrationTests
         }
 
         [Test]
-        public async Task Test_Tasks_CreateTask_ValidData()
+        public async System.Threading.Tasks.Task Test_Tasks_CreateTask_ValidData()
         {
+            //Arrange
 
-        }
+            var changedTask = new Data.Task()
+            {
+
+                Title = "New task",
+                Description = "There is no description",
+                Id = 150, 
+			};
+
+			//Act
+            await this.dbContext.AddAsync(changedTask);
+            await this.dbContext.SaveChangesAsync();
+
+			//Assert
+			this.dbContext = this.testDb.CreateDbContext();
+			var taskInDbAfter = this.dbContext.Tasks.Find(changedTask.Id);
+			Assert.AreEqual(changedTask.Title, taskInDbAfter.Title);
+		}
 
         [Test]
-        public async Task Test_Tasks_EditTask_ValidId()
+        public async System.Threading.Tasks.Task Test_Tasks_EditTask_ValidId()
         {
             //Arrange
             var editTask = this.testDb.EditTask;
@@ -72,18 +90,59 @@ namespace TaskBoard.WebAPI.IntegrationTests
         }
 
         [Test]
-        public async Task Test_Tasks_EditTask_InvalidId()
+        public async System.Threading.Tasks.Task Test_Tasks_EditTask_InvalidId()
         {
+            //Arrange
+            var fakeId = 1233;
+            var changedTask = new TaskBindingModel();
+
+			//Act
+			var putRespond = await this.httpClient.PutAsJsonAsync($"/api/tasks/{fakeId}", changedTask);
+
+			//Assert
+			Assert.AreEqual(HttpStatusCode.BadRequest, putRespond.StatusCode);
+		}
+
+        [Test]
+        public async System.Threading.Tasks.Task Test_Tasks_PartialEditTask_ValidId()
+        {
+            //Arrange
+            var editTask = this.testDb.EditTask;
+
+            var changedTitle = "Changed CSS Task";
+
+            var changedTask = new TaskBindingModel()
+            {
+                Title = changedTitle,
+                Description = editTask.Description,
+                Board = editTask.Board.Name
+            };
+
+
+            //Act
+            var putResponse = await this.httpClient.PutAsJsonAsync($"/api/tasks/{editTask.Id}", changedTask);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.NoContent, putResponse.StatusCode);
+
+            this.dbContext = this.testDb.CreateDbContext();
+            var taskInDbAfter = this.dbContext.Tasks.Find(editTask.Id);
+
+            Assert.AreEqual(changedTitle,taskInDbAfter.Title);
+            
         }
 
         [Test]
-        public async Task Test_Tasks_PartialEditTask_ValidId()
+        public async System.Threading.Tasks.Task Test_Tasks_DeleteTask_ValidId()
         {
-        }
+			//Arrange
+			var fakeId = 1233;
 
-        [Test]
-        public async Task Test_Tasks_DeleteTask_ValidId()
-        {
-        }
+			//Act
+			var putRespond = await this.httpClient.DeleteAsync($"/api/tasks/{fakeId}");
+
+			//Assert
+			Assert.AreEqual(HttpStatusCode.NotFound, putRespond.StatusCode);
+		}
     }
 }
