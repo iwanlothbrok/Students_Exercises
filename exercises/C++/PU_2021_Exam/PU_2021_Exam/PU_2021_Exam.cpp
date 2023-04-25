bf405b6aaf4c493d3168ebef979972ee8ecd5f88
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-04-25 13:06:12 +0300

    2021 PU exam

---
+++ exercises/C++/PU_2021_Exam/PU_2021_Exam/PU_2021_Exam.cpp
@@ -0,0 +1,62 @@
+﻿// PU_2021_Exam.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// we are creating the struct item 
+struct Item {
+    int id;
+    char name[20];
+    double price;
+    int quantity;
+    int type;
+    string code;
+
+} items[2000]; // setting to be array with max 2000 items
+
+
+void addItem(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        items[i].id = i + 1;
+        cin.ignore();
+
+        cout << "Please enter item's name: \n";
+        // cin getline because name its char[]
+        cin.getline(items[i].name, 20);
+
+        cout << "Please enter item's price: \n";
+        cin >> items[i].price; 
+        
+        cout << "Please enter item's quantity: \n";
+        cin >> items[i].quantity;
+
+        cout << "Please enter item's type: \n";
+        cin >> items[i].type;
+
+        items[i].code = to_string(items[i].id) + items[i].name[0] + items[i].name[1] + to_string(items[i].type);
+    }
+}
+
+
+int main()
+{
+    int n;
+    cout << "Add how many items you want: \n";
+    cin >> n;
+
+    addItem(n);
+
+
+}
+
+//За всеки артикул, трябва да се въведат(съхранят) :
+//     Пореден номер на въвеждане;
+// Име на артикул(знаков низ с дължина от 2 до 20 символа);
+// Единична цена(число с плаваща запетая, минимална цена 0.10);
+// Наличност от съответния артикул(цяло положително число);
+// Тип на артикул(типовете са от 1 до 5).
+//Програмата трябва да може да съхрани до 2000 артикула.
