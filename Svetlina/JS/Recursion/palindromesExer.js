60758d1668661b5581aea269ed66630fa9b3d596
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-04-26 14:14:06 +0300

    Palindromes and triangle exercises were added

---
+++ Svetlina/JS/Recursion/palindromesExer.js
@@ -0,0 +1,16 @@
+
+
+function isPalindrome(word){
+
+    if(word.length <= 1){
+        return true;
+    }
+
+    if (word[0] === word[word.length - 1]) {
+        return isPalindrome(word.slice(1, -1));
+      } else {
+        return false;
+      }
+}
+
+console.log(isPalindrome('eve'));
