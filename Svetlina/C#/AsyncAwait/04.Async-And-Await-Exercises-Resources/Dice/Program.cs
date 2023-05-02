e3b8d6689e1a5e3a4d5b91f012f2f6bf0cdd695d
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-02 17:43:27 +0300

    dice roll was added

---
+++ Svetlina/C#/AsyncAwait/04.Async-And-Await-Exercises-Resources/Dice/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+class Program
+{
+	public static void Main()
+	{
+		var result = DiceRollAsync();
+
+		Console.WriteLine($"Your dice roll result is {result}");
+	}
+
+	static async Task<int> DiceRollAsync()
+	{
+		Console.WriteLine("Dice rolling...");
+
+		int roll1 = await RollAsync();
+		int roll2 = await RollAsync();
+
+
+		return roll1 + roll2;
+	}
+
+	static async Task<int> RollAsync()
+	{
+		await Task.Delay(500);
+
+		return new Random().Next(1, 7);
+	}
+}
