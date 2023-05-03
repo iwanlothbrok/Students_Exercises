9031cd8d4661b8e5d58182689db4e000d43f277b
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-03 13:52:17 +0300

    Wiki download is working

---
+++ Svetlina/C#/AsyncAwait/04.Async-And-Await-Exercises-Resources/WikiDownload/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+	public static async Task Main()
+	{
+		string url = "https://developer.wikimedia.org";
+		string html = await DownloadHtmlAsync(url);
+		Console.WriteLine(html);
+	}
+
+	public static async Task<string> DownloadHtmlAsync(string url)
+	{
+		using (HttpClient client = new HttpClient())
+		{
+			HttpResponseMessage response = await client.GetAsync(url);
+			response.EnsureSuccessStatusCode();
+			string html = await response.Content.ReadAsStringAsync();
+			return html;
+		}
+	}
+}
