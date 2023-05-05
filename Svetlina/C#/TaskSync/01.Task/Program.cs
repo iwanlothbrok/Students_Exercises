96bd3f6a848b5d4182631b59246c8d50bbc507b0
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-05 14:05:41 +0300

    first two exercsises of task 02

---
+++ Svetlina/C#/TaskSync/01.Task/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+internal class Program
+{
+	static bool reserve = false;
+	static object locker = new object();
+	static void Main(string[] args)
+	{
+		// with lock
+		locker = new Thread(Reserve);
+		Reserve();
+		Reserve();
+	}
+	static void Reserve()
+	{
+		lock (locker)
+		{
+			if (!reserve)
+			{
+				reserve = true;
+				Console.WriteLine("Reserved");
+			}
+		}
+	}
+
+}
+
