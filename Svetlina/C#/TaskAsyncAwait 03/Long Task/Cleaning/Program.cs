a0fd7fcf1d494bc5e50c6092e7758f6497c3df52
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-05 16:36:16 +0300

    Task 03 third exercise

--- Svetlina/C#/TaskAsyncAwait 03/Long Task/Cleaning/Program.cs
+++ Svetlina/C#/TaskAsyncAwait 03/Long Task/Cleaning/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+	public static void Main()
+	{
+		double square = double.Parse(Console.ReadLine());
+
+		Task<double> vacuuming = Task.Run(() =>  square * 0.5 );
+		Task<double> washing = Task.Run(() => square * 1.5);
+
+		Task<double> cleaning = Task.Run(() => vacuuming.Result + washing.Result);
+
+		int hours = (int)(cleaning.Result / 60);
+		var minutes = cleaning.Result % 60;
+
+		Console.WriteLine($"Cleaning for {hours} hours and {minutes} minutes");
+	}
+
+}
