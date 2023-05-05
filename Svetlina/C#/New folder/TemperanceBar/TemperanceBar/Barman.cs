d4785e11e63a57170c17a7c124c1a9d9454d2052
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-05 17:49:52 +0300

    barman exercise

---
+++ Svetlina/C#/New folder/TemperanceBar/TemperanceBar/Barman.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TemperanceBar
+{
+    public class Barman : BarmanThread
+    {
+        // TODO: 
+        private string name;
+        Mocktail mocktail;
+
+        public Barman(string name, Mocktail mocktail)
+        {
+            this.name = name;
+            this.mocktail = mocktail;
+        }
+
+        public Mocktail Mocktail { get; set; }
+        public string Name { get; set; }
+        // The Barman class is a subclass of BarmanThread. 
+        // Upon initialization, it accepts and stores a string barman name and a Mocktail object.
+        // overrid the Work() method 
+        public override void Work()
+        {
+            Console.WriteLine($"{barman name} is preparing {mocktail name}");
+        }
+    }
+}
---
+++ Svetlina/C#/New folder/TemperanceBar/TemperanceBar/Mocktail.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace TemperanceBar
+{
+    public class Mocktail
+    { 
+        public Mocktail(string mocktailName, Ingredient[] ingrediants)
+        {
+            this.mocktailName = mocktailName;
+            this.ingrediants = ingrediants;
+        }
+
+        public string MocktailName { get; set; }
+        public Ingredient[] Ingredients { get; set; }
+        // TODO:
+        // On initialization, it takes and stores a string name and array of Ingredient objects.
+        // Override the ToString() method to return the name of the Mocktail.
+        public override string ToString()
+        {
+            return this.mocktailName;
+        }
+    }
+}
