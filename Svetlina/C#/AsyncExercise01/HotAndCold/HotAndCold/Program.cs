a83946bf6220612ed9e84754fcbb8661f169b6e8
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-05 14:19:53 +0300

    Async 01 first exercise added

---
+++ Svetlina/C#/AsyncExercise01/HotAndCold/HotAndCold/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+public class Program
+{
+	public static void Main()
+	{
+		Thread t1 = new Thread(PrintCold);
+		t1.Start();
+
+		PrintHot();
+	}
+	public static void PrintCold()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			Console.WriteLine(" Cold ");
+			Thread.Sleep(200);
+		}
+	}
+	public static void PrintHot()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			Console.WriteLine("Hot");
+			Thread.Sleep(200);
+		}
+	}
+}
