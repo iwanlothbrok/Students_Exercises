7f01e810101d3ee495c5d080b3105e416c57c90b
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-05 16:25:56 +0300

    Task 03 first and second exercises added

---
+++ Svetlina/C#/TaskAsyncAwait 03/Long Task/Long Task/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+	public static void Main()
+	{
+		Task task = new Task(Run);
+		task.Start();
+
+		for (int i = 0; i < 10; i++)
+		{
+			Console.WriteLine("----------------");
+		}
+
+		
+
+	}
+	static void Run()
+	{
+		while(true)
+		{
+			Console.WriteLine("Endless Loop...");
+		}
+	}
+}
