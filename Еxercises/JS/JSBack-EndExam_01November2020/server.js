f13b69fca2048c80e5619fc2bfe1fe1ec0b4619a
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-17 13:00:32 +0300

    trying to connect the mongoose

--- Еxercises/JS/JSBack-EndExam_01November2020/server.js
+++ Еxercises/JS/JSBack-EndExam_01November2020/server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 
 const {PORT} = require('./config/config');
-//require('./config/mongoose')(app);
-
+require('./config/mongoose');
 
 
 app.get('/',(req,res)=>{
+    res.send('hello from /')
 })
-
 
 app.listen(PORT, () => console.log(`server is running on ${PORT}... `));
