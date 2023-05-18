ef8aabe6348e655b2e4615b15b3be2ebc9a5acad
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-18 10:25:51 +0300

    User was created in the database, setting settings for the user

---
+++ Еxercises/JS/WildLifeExam/WildLife/models/User.js
@@ -0,0 +1,23 @@
+const { Schema, model } = required('mongoose');
+
+
+// similar to the db models in asp
+const userSchema = new Schema({
+    username: { type: String, require: true },
+    hashedPassword: { type: String, require: true }
+});
+
+// for sorting on param username, 1 for asc -1 for desc
+userSchema.index({ username: 1 }, {
+    unique: true,
+    // to become case insensetive
+    collation: {
+        locale: 'en',
+        strength: 2
+    }
+})
+
+// creates a Mongoose model named User 
+const User = model('User',userSchema);
+
+module.exports = User;
