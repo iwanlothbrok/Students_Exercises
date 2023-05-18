b61f6a2672519118a7714af1f8e999c1c979ef3f
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-18 09:55:44 +0300

    create database file

---
+++ Еxercises/JS/WildLifeExam/WildLife/config/database.js
@@ -0,0 +1,25 @@
+
+const mongoose = require('mongoose');
+
+const dbName = 'wildlife'
+const connectionString = `mongodb://localhost:27017/${dbName}`;
+
+module.exports = async (app) => {
+    try{
+        await mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+
+        mongoose.connection.on('error',(err) => {
+            console.log('Database error');
+        })
+        
+    } catch(err){
+        console.log('Error connection to database');
+        process.exit(1);
+    }
+
+
+}
