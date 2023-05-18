d0b2036d84d3dfa8d29b6c41a17ef5a4abcf5b90
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-18 09:45:03 +0300

    config is separated from the server

---
+++ Еxercises/JS/WildLifeExam/WildLife/config/express.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { create: handlebars } = require('express-handlebars');
+const session = require('express-session');
+
+module.exports = (app) => {
+     
+    app.engine('.hbs', handlebars({
+        extname: '.hbs',
+    }).engine);
+    
+    // setting to user hbs for views 
+    app.set('view engine', '.hbs');
+
+    // using the the express static for the static files
+    //using the express.static middleware.
+    app.use('/static', express.static('static'));
+
+    // setting the sessions and the cookies
+    app.use(session({
+        secret:'secret',
+        resave: 'false',//Specifies whether the session should be saved even if it's not modified.
+        saveUninitialized: true, //  a new session will be created even if no data has been added to it.
+        cookie:{
+            secure: 'auto' // (HTTPS or HTTP).
+        }
+    }));    
+
+    app.use(express.urlencoded({extended:true}));
+} 
--- Еxercises/JS/WildLifeExam/WildLife/index.js
+++ Еxercises/JS/WildLifeExam/WildLife/index.js
@@ -1,40 +1,16 @@
 const express = require('express');
-const { create: handlebars } = require('express-handlebars');
-const session = require('express-session');
-
-
+const expressConfig = require('./config/express')
+ 
 start();
 
 async function start() {
     const app = express();
 
-
-    app.engine('.hbs', handlebars({
-        extname: '.hbs',
-    }).engine);
-    
-    // setting to user hbs for views 
-    app.set('view engine', '.hbs');
-
-    // using the the express static for the static files
-    //using the express.static middleware.
-    app.use('/static', express.static('static'));
-
-    // setting the sessions and the cookies
-    app.use(session({
-        secret:'secret',
-        resave: 'false',//Specifies whether the session should be saved even if it's not modified.
-        saveUninitialized: true, //  a new session will be created even if no data has been added to it.
-        cookie:{
-            secure: 'auto' // (HTTPS or HTTP).
-        }
-    }));    
-
-    app.use(express.urlencoded({extended:true}));
+    expressConfig(app); 
 
     // renders the home view
     app.get('/',(req,res) => res.render('home', {layout : false}));
-
+    
     // starting server on port 3001
     app.listen(3001, () => 'Server is listening');
 }
