41f33800ab82d21bca6e2f09ff001dfe19c00a10
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-18 13:06:25 +0300

    server functionality was improved

--- Еxercises/JS/WildLifeExam/WildLife/index.js
+++ Еxercises/JS/WildLifeExam/WildLife/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const expressConfig = require('./config/express')
- 
+const expressConfig = require('./config/express');
+const databaseConfig = require('./config/database');
+const routesConfig = require('./config/routes');
+
+
 start();
 
 async function start() {
     const app = express();
 
     expressConfig(app); 
-
+    await databaseConfig(app);
+    routesConfig(app);
+    
     // renders the home view
     app.get('/',(req,res) => res.render('home', {layout : false}));
     
     // starting server on port 3001
-    app.listen(3001, () => 'Server is listening');
+    app.listen(3001, () =>console.log('Server is listening'));
 }
