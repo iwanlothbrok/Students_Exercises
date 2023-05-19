bda0ceb81a7718a65a3b29fd4c57540f1fc760a4
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-19 11:21:43 +0300

    guard was added and the frontend was changed

--- Еxercises/JS/WildLifeExam/WildLife/index.js
+++ Еxercises/JS/WildLifeExam/WildLife/index.js
@@ -13,9 +13,6 @@ async function start() {
     await databaseConfig(app);
     routesConfig(app);
 
-    // renders the home view
-    app.get('/', (req, res) => res.render('home', { layout: false }));
-
     // starting server on port 3001
     app.listen(3001, () => console.log('Server is listening'));
 }
