beb94016a4f741bfba6c0ecd8a644e001876b8a0
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-05-19 09:15:46 +0300

    POST register was added

--- Еxercises/JS/WildLifeExam/WildLife/controllers/auth.js
+++ Еxercises/JS/WildLifeExam/WildLife/controllers/auth.js
@@ -1,11 +1,29 @@
+
+const { register, login } = require('../services/userService')
 const router = require('express').Router();
 
-router.get('/register',(req,res) =>{
-    res.render('register',  {layout : false});
-})
+router.get('/register', (req, res) => {
+    res.render('register', { layout: false });
+});
 
-router.get('/login',(req,res) =>{
-    res.render('login', {layout : false});
+ router.post('/register', async (req, res) => {
+    try {
+        if (req.body.password != req.body.repass) {
+            throw new Error('Password do not match!')
+        }
+       const user = await register(req.body.username,req.body.password);
+
+       req.session.user = user;
+
+       res.render('/');
+    }
+    catch (err) {
+        res.render('register', { layout: false });
+    }
+});
+
+router.get('/login', (req, res) => {
+    res.render('login', { layout: false });
 })
 
 module.exports = router;
