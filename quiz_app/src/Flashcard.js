d9a41035debbade28ae77a176b55016a69a5c46a
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-07-04 16:38:02 +0300

    Flashcard with resize and max height

--- quiz_app/src/Flashcard.js
+++ quiz_app/src/Flashcard.js
@@ -1,25 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 export default function Flashcard({ flashcards }) {
-    const [flip, setFlip] = useState(false);
+    const [flip, setFlip] = useState(false)
+    const [height, setHeight] = useState('initial')
 
+    const frontEl = useRef()
+    const backEl = useRef()
 
-
+    function setMaxHeight() {
+        const frontHeight = frontEl.current.getBoundingClientRect().height
+        const backHeight = backEl.current.getBoundingClientRect().height
+        setHeight(Math.max(frontHeight, backHeight, 100))
+    }
+    useEffect(setMaxHeight, [flashcards.questions, flashcards.answer, flashcards.options])
+    useEffect(() => {
+        window.addEventListener('resize', setMaxHeight)
+        return () => window.removeEventListener('resize', setMaxHeight)
+    }, [])
     return (
         <div
+            className={`card ${flip ? 'flip' : ''}`}
+            style={{ height: height }}
             onClick={() => setFlip(!flip)}
-            className={`card' ${flip ? 'flip' : ''}`}>
-
-            <div className='front'>
+        >
+            <div className="front" ref={frontEl}>
                 {flashcards.question}
-                <div className='flashcard-options'>
-                    {flashcards.options.map(op => {
-                        return <div className='flashcard-option'>{op}</div>
+                <div className="flashcard-options">
+                    {flashcards.options.map(option => {
+                        return <div className="flashcard-option" key={option}>{option}</div>
                     })}
                 </div>
             </div>
-
-            <div className='back'>{flashcards.answer}</div>
+            <div className="back" ref={backEl}>{flashcards.answer}</div>
         </div>
     )
 }
